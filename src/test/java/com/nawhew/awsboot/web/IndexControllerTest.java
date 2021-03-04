75ccad581cab873ef8640b3ec95700221b8b1aa9
wehwan <we_hwan@naver.com>
2021-03-05 00:18:40 +0900

    test: index test 시큐리티 적용

--- src/test/java/com/nawhew/awsboot/web/IndexControllerTest.java
+++ src/test/java/com/nawhew/awsboot/web/IndexControllerTest.java
@@ -1,30 +1,43 @@
 package com.nawhew.awsboot.web;
 
+import com.nawhew.awsboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(controllers = HelloController.class
+        , excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class IndexControllerTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     @DisplayName("index 페이지 로딩")
-    public void index() {
-        // when
-        String body = this.restTemplate.getForObject("/", String.class);
-
-        // then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+    public void index() throws Exception {
+        // when & then
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
